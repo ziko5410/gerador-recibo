2bda03e613e84f0c94e631e59ae9b94d261951dc
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-02-03 19:51:25 -0300

    refactor: Schema definition script

--- assets/schema/000010.sql
+++ assets/schema/000010.sql
@@ -1,39 +1,33 @@
-drop database if exists recibeira;
-
-create database recibeira default character set utf8 collate utf8_unicode_ci;
-
-use recibeira;
-
-create table usuarios (
-	id int AUTO_INCREMENT NOT NULL UNIQUE,
-	username varchar(50) NOT NULL,
-	user_email varchar(50) NOT NULL,
-	user_password varchar(255) NOT NULL,
-	email_hash varchar(40) NOT NULL,
+CREATE TABLE IF NOT EXISTS usuarios (
+	id INT AUTO_INCREMENT NOT NULL UNIQUE,
+	username VARCHAR(50) NOT NULL,
+	user_email VARCHAR(50) NOT NULL,
+	user_password VARCHAR(255) NOT NULL,
+	email_hash VARCHAR(40) NOT NULL,
 	email_verified tinyint(1) NOT NULL,
-	acc_code int(6) NOT NULL,
-	user_id int(10),
-	user_token varchar(64),
+	acc_code INT(6) NOT NULL,
+	user_id INT(10),
+	user_token VARCHAR(64),
 	PRIMARY KEY (id)
 );
 
-create table user_profiles (
-	profile_id int AUTO_INCREMENT NOT NULL,
-  user_id int NOT NULL,
-  profile_name varchar(30) NOT NULL,
-  locador varchar(50) NOT NULL,
-  locatario varchar(50) NOT NULL,
-  inicio_contrato date,
-  termino_contrato date,
-  valor_aluguel decimal(7,2) NOT NULL,
-  rua varchar(40) NOT NULL,
+create table IF NOT EXISTS user_profiles (
+	profile_id INT AUTO_INCREMENT NOT NULL,
+  user_id INT NOT NULL,
+  profile_name VARCHAR(30) NOT NULL,
+  locador VARCHAR(50) NOT NULL,
+  locatario VARCHAR(50) NOT NULL,
+  inicio_contrato DATE,
+  termino_contrato DATE,
+  valor_aluguel DECIMAL(7,2) NOT NULL,
+  rua VARCHAR(40) NOT NULL,
   numero_casa int(5) NOT NULL,
-  bairro varchar(40) NOT NULL,
-  cidade varchar(30) NOT NULL,
-  casa_fundo tinyint(1) NOT NULL,
-  referente_from date NOT NULL,
-  referente_to date NOT NULL,
-  data_recibo date NOT NULL,
+  bairro VARCHAR(40) NOT NULL,
+  cidade VARCHAR(30) NOT NULL,
+  casa_fundo TINYINT(1) NOT NULL,
+  referente_from DATE NOT NULL,
+  referente_to DATE NOT NULL,
+  data_recibo DATE NOT NULL,
   PRIMARY KEY (profile_id),
   FOREIGN KEY (user_id) REFERENCES usuarios(id)
 );
