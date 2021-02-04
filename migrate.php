81f57a16ca98f49d71d78bd45ea38adfca413bb4
Allex Rodrigues <allexxrodriguess@gmail.com>
2021-02-03 22:36:12 -0300

    feat: Add schema executor

---
+++ migrate.php
@@ -0,0 +1,32 @@
+<?php
+require_once "assets/scripts/connect.php";
+
+function parseScript($script) {
+
+  $result = array();
+  $delimiter = ';';
+  while(strlen($script) && preg_match('/((DELIMITER)[ ]+([^\n\r])|[' . $delimiter . ']|$)/is', $script, $matches, PREG_OFFSET_CAPTURE)) {
+    if (count($matches) > 2) {
+      $delimiter = $matches[3][0];
+      $script = substr($script, $matches[3][1] + 1);
+    } else {
+      if (strlen($statement = trim(substr($script, 0, $matches[0][1])))) {
+        $result[] = $statement;
+      }
+      $script = substr($script, $matches[0][1] + 1);
+    }
+  }
+
+  return $result;
+
+}
+
+function executeScriptFile($fileName, $dbConnection) {
+  $script = file_get_contents($fileName);
+  $statements = parseScript($script);
+  foreach($statements as $statement) {
+    mysqli_query($dbConnection, $statement);
+  }
+}
+
+executeScriptFile(__DIR__.'/schema.sql', $BD_Connection);
